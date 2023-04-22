4426533f34516207d0601831a13326c64c3e9b2e
elnavut <levantu.nd1997@gmail.com>
2023-04-23 00:31:29 +0700

    comment out code

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,10 +15,10 @@ export default function Home() {
       </Head>
       <div className='mx-auto w-full max-w-full relative z-10 main'>
         <TopSection />
-        <RecommendedGames isShowShape />
+        {/* <RecommendedGames isShowShape />
         <NewGames isShowShape />
         <PopularGames isShowShape />
-        <PopularCategories isShowShape />
+        <PopularCategories isShowShape /> */}
         <Description />
         <PopularTags />
       </div>
