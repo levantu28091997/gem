d966327c48c260a680e2cd2cca8c7ec3d3c39cef
elnavut <levantu.nd1997@gmail.com>
2023-04-26 15:48:09 +0700

    append preload & avoid invisible text during font loading

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,21 +1,21 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-    return (
-        <Html lang="en">
-            <Head>
+  return (
+    <Html lang="en">
+      <Head>
 
-                <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@300;400;500;600;800&display=swap" rel="stylesheet" />
-                <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet"></link>
-                <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;600&display=swap" rel="stylesheet"></link>
-                <link href="https://fonts.googleapis.com/css2?family=Changa+One&display=swap" rel="stylesheet"></link>
-                <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet"></link>
-                <link href="https://fonts.googleapis.com/css2?family=Genos:wght@500&display=swap" rel="stylesheet"></link>
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    )
+        <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@500;600;800&display=swap" rel="stylesheet" />
+        <link rel="preload" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"></link>
+        <link rel="preload" href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;600&display=swap"></link>
+        <link rel="preload" href="https://fonts.googleapis.com/css2?family=Changa+One&display=swap"></link>
+        <link rel="preload" href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"></link>
+        <link rel="preload" href="https://fonts.googleapis.com/css2?family=Genos:wght@500&display=swap"></link>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
