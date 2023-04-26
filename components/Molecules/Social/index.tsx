b11fbbb73e968741d12855ee20ce80a0f6250189
elnavut <levantu.nd1997@gmail.com>
2023-04-26 16:36:11 +0700

    fix: Links must have discernible text

--- components/Molecules/Social/index.tsx
+++ components/Molecules/Social/index.tsx
@@ -2,34 +2,34 @@ import React from 'react'
 import styles from "./Social.module.scss"
 import cs from '@/utils/cs'
 import {
-    IconSocialFacebook,
-    IconSocialInsta,
-    IconSocialTiktok,
-    IconSocialGoogle,
-    IconSocialLinkedin
+  IconSocialFacebook,
+  IconSocialInsta,
+  IconSocialTiktok,
+  IconSocialGoogle,
+  IconSocialLinkedin
 } from "@/components/Atoms/Icons"
 
 
 const Social = () => {
-    return (
-        <div className={cs([styles.Social, "flex social sm:mb-5"])}>
-            <a className='mr-3 sm:mr-10 w-3.5 sm:w-auto' href="https://www.facebook.com/" target="_blank" rel="noreferrer">
-                <IconSocialFacebook />
-            </a>
-            <a className='mr-3 sm:mr-10 w-3.5 sm:w-auto' href="https://www.instagram.com/" target="_blank" rel="noreferrer">
-                <IconSocialInsta />
-            </a>
-            <a className='mr-3 sm:mr-10 w-3.5 sm:w-auto' href="https://www.tiktok.com/" target="_blank" rel="noreferrer">
-                <IconSocialTiktok />
-            </a>
-            <a className='mr-3 sm:mr-10 w-3.5 sm:w-auto' href="https://www.google.com/" target="_blank" rel="noreferrer">
-                <IconSocialGoogle />
-            </a>
-            <a className='w-3.5 sm:w-auto' href="https://www.linkedin.com/" target="_blank" rel="noreferrer">
-                <IconSocialLinkedin />
-            </a>
-        </div>
-    )
+  return (
+    <div className={cs([styles.Social, "flex social sm:mb-5"])}>
+      <a className='mr-3 sm:mr-10 w-3.5 sm:w-auto' aria-label="Link to facebook" href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+        <IconSocialFacebook />
+      </a>
+      <a className='mr-3 sm:mr-10 w-3.5 sm:w-auto' aria-label="Link to instagram" href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+        <IconSocialInsta />
+      </a>
+      <a className='mr-3 sm:mr-10 w-3.5 sm:w-auto' aria-label="Link to tiktok" href="https://www.tiktok.com/" target="_blank" rel="noreferrer">
+        <IconSocialTiktok />
+      </a>
+      <a className='mr-3 sm:mr-10 w-3.5 sm:w-auto' aria-label="Link to google" href="https://www.google.com/" target="_blank" rel="noreferrer">
+        <IconSocialGoogle />
+      </a>
+      <a className='w-3.5 sm:w-auto' aria-label="Link to linkedin" href="https://www.linkedin.com/" target="_blank" rel="noreferrer">
+        <IconSocialLinkedin />
+      </a>
+    </div>
+  )
 }
 
 export default Social
