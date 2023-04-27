aa1197b520baa1f78933d0a858c790ae41699ab4
elnavut <levantu.nd1997@gmail.com>
2023-04-27 11:53:27 +0700

    import dynamic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,27 @@
-import Description from '@/components/Organisms/Description';
+// import Description from '@/components/Organisms/Description';
 import NewGames from '@/components/Organisms/NewGames';
 import PopularCategories from '@/components/Organisms/PopularCategories';
 import PopularGames from '@/components/Organisms/PopularGames';
-import PopularTags from '@/components/Organisms/PopularTags';
+// import PopularTags from '@/components/Organisms/PopularTags';
 import RecommendedGames from '@/components/Organisms/RecommendedGames';
-import TopSection from '@/components/Organisms/TopSection';
+// import TopSection from '@/components/Organisms/TopSection';
 import Head from 'next/head';
+import dynamic from 'next/dynamic'
+
+const TopSection = dynamic(() => import('@/components/Organisms/TopSection'), {
+  loading: () => <p>Loading...</p>,
+  ssr: false
+})
+
+const Description = dynamic(() => import('@/components/Organisms/Description'), {
+  loading: () => <p>Loading...</p>,
+  ssr: false
+})
+
+const PopularTags = dynamic(() => import('@/components/Organisms/PopularTags'), {
+  loading: () => <p>Loading...</p>,
+  ssr: false
+})
 
 export default function Home() {
   return (
