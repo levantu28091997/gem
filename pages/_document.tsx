82f9b1c2f0c9a37581d9a16bf0690faa3180cec4
elnavut <levantu.nd1997@gmail.com>
2023-04-27 10:18:09 +0700

    change to used local font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,14 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* <link rel="prefetch" as="font" href="https://fonts.googleapis.com/css2?family=Barlow:wght@500;600;800&display=swap" />
-        <link rel="prefetch" as="font" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"></link>
-        <link rel="prefetch" as="font" href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;600&display=swap"></link>
-        <link rel="prefetch" as="font" href="https://fonts.googleapis.com/css2?family=Changa+One&display=swap"></link>
-        <link rel="prefetch" as="font" href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"></link>
-        <link rel="prefetch" as="font" href="https://fonts.googleapis.com/css2?family=Genos:wght@500&display=swap"></link> */}
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
