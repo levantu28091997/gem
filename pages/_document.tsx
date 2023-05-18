dc079b1a8bdd1fba29c79b7f23f0e0d61a589957
elnavut <levantu.nd1997@gmail.com>
2023-05-18 14:31:13 +0700

    comment font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,12 +5,12 @@ export default function Document() {
         <Html lang="en">
             <Head>
 
-                <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@300;400;500;600;800&display=swap" rel="stylesheet" />
+                {/* <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@300;400;500;600;800&display=swap" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet"></link>
                 <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;600&display=swap" rel="stylesheet"></link>
                 <link href="https://fonts.googleapis.com/css2?family=Changa+One&display=swap" rel="stylesheet"></link>
                 <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet"></link>
-                <link href="https://fonts.googleapis.com/css2?family=Genos:wght@500&display=swap" rel="stylesheet"></link>
+                <link href="https://fonts.googleapis.com/css2?family=Genos:wght@500&display=swap" rel="stylesheet"></link> */}
             </Head>
             <body>
                 <Main />
